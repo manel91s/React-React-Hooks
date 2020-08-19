05ec5ac50adf143210388cfdd4f220b7e2c8ef06
Manel <manel.aguilera91@gmail.com>
2020-08-19 17:35:53 +0200

    maquetando el componente de formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,7 +2,41 @@ import React from 'react';
 
 const Formulario = () => {
     return ( 
-        <h1>Formulario aqui</h1>
+        <form
+            className="col-12"
+        >
+            <fieldset className="text-center">
+                <legend>Busca bebidas por categoria o ingrediente</legend>
+            </fieldset>
+
+            <div className="row mt-4">
+                <div className="col-md-4">
+                    <input
+                        name="nombre"
+                        className="form-control"
+                        type="text"
+                        placeholder="Buscar por ingrediente"
+                    />
+                </div>
+
+                <div className="col-md-4">
+                    <select
+                        className="form-control"
+                        name="categoria"
+                    >
+                        <option value="">-- Selecciona Categoría --</option>
+                    </select>
+                </div>
+
+                <div className="col-md-4">
+                    <input
+                        type="submit"
+                        className="btn btn-block btn-primary"
+                        value="Buscar Bebidas"
+                    />
+                </div>
+            </div>
+        </form>
      );
 }
  
