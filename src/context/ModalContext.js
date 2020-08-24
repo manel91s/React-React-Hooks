7a828414c4ac2e7be2cb217f51c19878b9949339
Manel <manel.aguilera91@gmail.com>
2020-08-24 13:29:06 +0200

    llamado a la api cada vez que pulsamos sobre la bebida y que nos traiga el detalle

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -8,6 +8,22 @@ const ModalProvider = (props) => {
 
     //State del providers
     const [ idreceta, guardarIdReceta ] = useState(null);
+
+    //Una vez que tenemos una receta, llamar la api
+
+    useEffect( () => {
+        const obtenerReceta = async () => {
+            if(!idreceta) return;
+            
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idreceta}`;
+
+            const resultado = await axios.get(url);
+
+            console.log(resultado.data.drinks[0]);
+        }
+
+        obtenerReceta();
+    }, [idreceta])
     return (  
         <ModalContext.Provider
             value={{
